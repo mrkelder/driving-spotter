fa455f568bf7fe483b1961ce6eb0509cc49872b9
Vlad Aheiev <vageev06@gmail.com>
2025-01-19 03:02:42 -0500

    added location detection to the report

--- app/(tabs)/report.tsx
+++ app/(tabs)/report.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, Platform, View, Text, TextInput } from "react-native";
 import { Image } from "expo-image";
 import { HelloWave } from "@/components/HelloWave";
@@ -8,12 +8,58 @@ import { ThemedView } from "@/components/ThemedView";
 import Input from "@/components/Input";
 import RippleButton from "@/components/RippleButton";
 import { Link, useRouter } from "expo-router";
+import * as Location from "expo-location";
 
 export default function ReportScreen() {
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState(""); // Added state for email
-  const router = useRouter();
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [locationAddress, setLocationAddress] = useState<string>("");
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  useEffect(() => {
+    async function fetchReports() {
+      let response = await fetch(
+        `https://nominatim.openstreetmap.org/reverse?lat=${location?.coords.latitude}&lon=${location?.coords.longitude}&format=json`
+      );
+
+      const jsonResponse = (await response.json()) as {
+        address: {
+          road: string;
+          city: string;
+          state: string;
+          postcode: string;
+        };
+      };
+
+      setLocationAddress(
+        `${jsonResponse.address.road}, ${jsonResponse.address.city}, ${jsonResponse.address.state}, ${jsonResponse.address.postcode}`
+      );
+    }
+    if (location) fetchReports();
+  }, [location]);
+
+  let text = "Waiting...";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   return (
     <View
@@ -24,7 +70,12 @@ export default function ReportScreen() {
         justifyContent: "center",
         gap: 8,
         height: "100%",
+        padding: 8,
       }}
-    ></View>
+    >
+      <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+        Your location is {locationAddress}
+      </Text>
+    </View>
   );
 }
