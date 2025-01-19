21419411fdafd30d754e1b565318006ed6a9b1e0
Vlad Aheiev <vageev06@gmail.com>
2025-01-19 01:30:22 -0500

    created an empty report page

---
+++ app/(tabs)/report.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { StyleSheet, Platform, View, Text, TextInput } from "react-native";
+import { Image } from "expo-image";
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import Input from "@/components/Input";
+import RippleButton from "@/components/RippleButton";
+import { Link, useRouter } from "expo-router";
+
+export default function ReportScreen() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState(""); // Added state for email
+  const router = useRouter();
+
+  return (
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 8,
+        height: "100%",
+      }}
+    ></View>
+  );
+}
